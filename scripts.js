5e4b331ae03f247761a0dc4580c97febdd41d0cb
Morello Mariana <morello.mariana.dg@gmail.com>
2024-12-21 21:33:12 -0300

    tp-final

--- scripts.js
+++ scripts.js
@@ -2,38 +2,38 @@ console.log("Hola")
 
 const productos = [
     {
-        nombre: "Skate Lab Okusai",
-        descripcion: "Tabla cóncava con doble kick, compuesto por laminas de madera y resina epoxi. <br>Truck reforzados de aluminio <br> Bujes 95 A <br> Rulemanes Abec 9 <br> Ruedas de Uretano 52 a 54 mm. <br> Madera Guatambú: resiste hasta 90 kg",
+        nombre: "Skate Lab Retro",
+        descripcion: "Tabla cóncava con doble kick, compuesto por laminas de madera y resina epoxi. <br>Truck reforzados de aluminio <br> Bujes 95 A <br> Rulemanes Abec 9 <br> Ruedas de Uretano 52 a 54 mm. <br> Madera Guatambú: resiste hasta 90 kg.",
         imagen:"imagenes/skate_lab_01.jpg",
         precio: 100000,
     },
     {
         nombre: "Skate Lab Bicolor",
-        descripcion: "Tabla cóncava con doble kick, compuesto por laminas de madera y resina epoxi. <br>Truck reforzados de aluminio <br> Bujes 95 A <br> Rulemanes Abec 9 <br> Ruedas de Uretano 52 a 54 mm. <br> Madera Guatambú: resiste hasta 90 kg",
+        descripcion: "Tabla cóncava con doble kick, compuesto por laminas de madera y resina epoxi. <br>Truck reforzados de aluminio <br> Bujes 95 A <br> Rulemanes Abec 9 <br> Ruedas de Uretano 52 a 54 mm. <br> Madera Guatambú: resiste hasta 90 kg.",
         imagen:"imagenes/skate_lab_02.jpg",
         precio: 75000,
     },
     {
         nombre: "Skate Lab Canadá",
-        descripcion: "Tabla cóncava con doble kick, compuesto por laminas de madera y resina epoxi. <br>Truck reforzados de aluminio <br> Bujes 95 A <br> Rulemanes Abec 9 <br> Ruedas de Uretano 52 a 54 mm. <br> Madera Guatambú: resiste hasta 90 kg",
+        descripcion: "Tabla cóncava con doble kick, compuesto por laminas de madera y resina epoxi. <br>Truck reforzados de aluminio <br> Bujes 95 A <br> Rulemanes Abec 9 <br> Ruedas de Uretano 52 a 54 mm. <br> Madera Guatambú: resiste hasta 90 kg.",
         imagen:"imagenes/skate_lab_03.jpg",
         precio:80000,
     },
     {
         nombre: "Skate Lab Pride",
-        descripcion: "Tabla cóncava con doble kick, compuesto por laminas de madera y resina epoxi. <br>Truck reforzados de aluminio <br> Bujes 95 A <br> Rulemanes Abec 9 <br> Ruedas de Uretano 52 a 54 mm. <br> Madera Guatambú: resiste hasta 90 kg",
+        descripcion: "Tabla cóncava con doble kick, compuesto por laminas de madera y resina epoxi. <br>Truck reforzados de aluminio <br> Bujes 95 A <br> Rulemanes Abec 9 <br> Ruedas de Uretano 52 a 54 mm. <br> Madera Guatambú: resiste hasta 90 kg.",
         imagen:"imagenes/skate_lab_04.jpg",
         precio:120000,
 
     },
     {
-        nombre: "Longboard Cruiser",
+        nombre: "Longboard Cruis",
         descripcion: "Tabla cóncava compuesta por 7 laminas de madera y resina epoxi. <br> Largo: 80cm <br> Ancho: 25cm <br> WB: 42,5cm",
         imagen:"imagenes/long_lab_01.jpg",
         precio:120000,
     },
     {
-        nombre: "Longboard Rider Pro",
+        nombre: "Longboard Rider",
         descripcion: "Tabla cóncava compuesta por 7 laminas de madera y resina epoxi. <br> Largo: 80cm <br> Ancho: 25cm <br> WB: 42,5cm",
         imagen:"imagenes/long_lab_02.jpg",
         precio:135000,
@@ -52,45 +52,45 @@ const productos = [
     },
     {
         nombre:"Casco Lab",
-        descripcion: "Plástico exterior: Polipropileno. <br> Material interior: Goma Eva con espuma y poliéster <br> No removible. <br>Tiras de Polyester nylon <br> Orificios de ventilación",
+        descripcion: "Plástico exterior: Polipropileno. <br> Material interior: Goma Eva con espuma y poliéster <br> No removible. <br>Tiras de Polyester nylon <br> Orificios de ventilación.",
         imagen:"imagenes/casco.jpg",
         precio:36900,
     },
     {
-        nombre:"Protecciones",
+        nombre:"Protecciones Lab",
         descripcion:"Kit de protecciones para skate o longboard.<br> Talle S/M.<br> Incluye: set de 2 rodilleras + set de 2 coderas + set de 2 muñequeras.",
         imagen:"imagenes/protecciones.jpg",
         precio:69300,
 
     },
-    {   nombre: "Lija de papel",
-        descripcion:"Lija adhesiva doble AA de 23x85cm de papel.",
+    {   nombre: "Lija de Papel Lab",
+        descripcion:"Lija adhesiva doble AA de 23x85cm de papel. <br> Apto para Skates y Longboards. <br>Más modelos disponibles próximamente por nuestra web. <br> Consulte por diseños personalizados.",
         imagen:"imagenes/lija.jpg",
         precio:9000,
     },
     {
         nombre:"Trucks de Skate",
-        descripcion:" Trucks de Skate de alta calidad para profesionales. <br> Construidos en fundición de aluminio y ejes de acero templado. <br> Es ideal para tablas de 7.50″, 7.75″ y 8″",
+        descripcion:" Trucks de Skate de alta calidad para profesionales. <br> Construidos en fundición de aluminio y ejes de acero templado. <br> Es ideal para tablas de 7.50″, 7.75″ y 8″.",
         imagen:"imagenes/trucks.jpg",
         precio:37000,
     },
     {
         nombre:"Gorra Santa Cruz",
-        descripcion:"Gorra de gabardina negra",
+        descripcion:"Gorra de gabardina negra con parche en el frente.",
         imagen:"imagenes/gorra.jpg",
         precio:25000,
 
     },
     {
         nombre:"Gorro Santa Cruz",
-        descripcion:"Gorro de lana gris tejido con parche en el frente",
+        descripcion:"Gorro de lana gris tejido con parche en el frente.",
         imagen:"imagenes/gorro_lana.jpg",
         precio:25000,
         
     },
     {
-        nombre:"Remera Santa Cruz",
-        descripcion:"Remera de algodón 100% premium",
+        nombre:"Remera Vans",
+        descripcion:"Remera de algodón 100% premium.",
         imagen:"imagenes/remera.jpg",
         precio:15000,
         
@@ -191,7 +191,7 @@ function irAPagar(){
 
 
     if(listaCarrito.innerText=== ""){
-        mensajePagarCarrito.innerText = "No has seleccionado ningun Producto";
+        mensajePagarCarrito.innerText = "*No has seleccionado ningun Producto";
 
     } else{
         window.location.href = "./pagos.html"
